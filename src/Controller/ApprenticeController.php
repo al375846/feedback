fcb5ba0fe4324f5ced9db0c8793b42d0b35e2612
al375846 <al375846@uji.es>
2021-03-26 11:09:07 +0100

    apprentice controller refactorizado

--- src/Controller/ApprenticeController.php
+++ src/Controller/ApprenticeController.php
@@ -30,9 +30,9 @@ use Symfony\Component\HttpFoundation\HeaderUtils;
 
 class ApprenticeController extends AbstractController
 {
-    #[Route('/api/apprentice/{username}/publication', name: 'apprentice_get_publication', methods: ['GET'])]
+    #[Route('/api/apprentice/publication', name: 'apprentice_get_publication', methods: ['GET'])]
     /**
-     * @Route("/api/apprentice/{username}/publication", name="apprentice_get_publication", methods={"GET"})
+     * @Route("/api/apprentice/publication", name="apprentice_get_publication", methods={"GET"})
      * @OA\Response(response=200, description="Gets alls publications of an apprentice",
      *     @OA\JsonContent(type="object",
      *     @OA\Property(property="publications", type="array", @OA\Items(type="object",
@@ -47,14 +47,18 @@ class ApprenticeController extends AbstractController
      *     @OA\Property(property="images", type="array", @OA\Items(type="string")),
      *     @OA\Property(property="date", type="string", format="date-time")
      * ))))
-     * @OA\Response(response=404, description="Not found",
+     * @OA\Response(response=401, description="Unauthorized",
+     *     @OA\JsonContent(type="object",
+     *     @OA\Property(property="error", type="string")
+     * ))
+     * @OA\Response(response=409, description="Conflict",
      *     @OA\JsonContent(type="object",
      *     @OA\Property(property="error", type="string")
      * ))
      * @OA\Tag(name="Apprentices")
      * @Security(name="Bearer")
      */
-    public function getPublicationsUser($username): Response
+    public function getPublicationsUser(Request $request): Response
     {
         //Inicialiazamos los normalizadores y los codificadores para serialiar y deserializar
         $encoders = [new XmlEncoder(), new JsonEncoder()];
@@ -65,25 +69,32 @@ class ApprenticeController extends AbstractController
 
         //Trabajamos los datos como queramos
         $doctrine = $this->getDoctrine();
+
         //Obtenemos al aprendiz
-        try {
-        $user = $doctrine->getRepository(User::class)->findBy(['username'=>$username])[0];
-        $apprentice = $doctrine->getRepository(Apprentice::class)->findBy(['userdata'=>$user])[0];
-        } catch (\Throwable $e) {
-            $response=array('error'=>'Usuario no existe');
-            return new JsonResponse($response,404);
+        $token = explode(" ", $request->headers->get('Authorization'))[1];
+        $tokenParts = explode(".", $token);
+        $tokenPayload = base64_decode($tokenParts[1]);
+        $jwtPayload = json_decode($tokenPayload);
+        $username = $jwtPayload->username;
+        $apprentice = $doctrine->getRepository(Apprentice::class)->findOneBy(['username'=>$username]);
+
+        //Comprobamos que el usuario es aprendiz
+        if ($apprentice == null) {
+            $response=array('error'=>'El usuario no es un aprendiz');
+            return new JsonResponse($response, 409);
         }
+
         //Obtenemos las publicaciones
         $publications = $doctrine->getRepository(Publication::class)->findBy(['apprentice'=>$apprentice]);
 
         //Serializamos para poder mandar el objeto en la respuesta
-        $data = $serializer->serialize($publications, 'json',
-            [AbstractNormalizer::GROUPS => ['publications'], AbstractNormalizer::IGNORED_ATTRIBUTES => ['apprentice']]);
+        $data = $serializer->serialize($publications, 'json', [
+            AbstractNormalizer::GROUPS => ['publications'],
+            AbstractNormalizer::IGNORED_ATTRIBUTES => ['apprentice']
+        ]);
 
-        //Puede tener los atributos que se quieran
-        $response=array(
-            'publications'=>json_decode($data)
-        );
+        //Creamos la respuesta
+        $response=array('publications'=>json_decode($data));
 
         return new JsonResponse($response, 200);
     }
